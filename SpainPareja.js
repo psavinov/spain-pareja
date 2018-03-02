e58df2987be1df4f10b64375727ead415375196a
pavel-savinov <pavel.savinov@liferay.com>
2018-03-02 12:27:50 +0100

    initial commit

---
+++ SpainPareja.js
@@ -0,0 +1,167 @@
+var Spain = {
+	"data": {
+		"andalucia": {
+			"name": "Андалусия",
+			"application": "http://www.juntadeandalucia.es/export/drupaljda/Infancia_Familias_001076-A00-V03-00_20131108.pdf",
+			"law": "http://www.juntadeandalucia.es/organismos/igualdadypoliticassociales/areas/infancia-familias/parejas-hecho/paginas/faqs-parejas-hecho.html",
+			"where": "Ayuntamiento в Вашем населённом пункте, либо Delegación Provincial para la Igualdad y Bienestar Social Вашей провинции"
+		},
+		"aragon": {
+			"name": "Арагон",
+			"application": "http://www.aragon.es/estaticos/GobiernoAragon/Departamentos/CiudadaniaDerechosSociales/Areas/BienestarSocialFamilia/Areas/03_Familia/Carpeta%20de%20documentos/solicitud-inscripcion-parejas-2016.2.pdf",
+			"law": "http://www.aragon.es/DepartamentosOrganismosPublicos/Departamentos/CiudadaniaDerechosSociales/AreasTematicas/CDS_Apoyo-a-las-Familias/BSF_03_Familia/SERVICIOS-Y-PRESTACIONES/ci.Registro-de-Parejas-Estables-no-Casadas.detalleDepartamento?channelSelected=27f450b7c3035510VgnVCM2000002f551bacRCRD",
+			"where": "Registro Civil в Вашем населённом пункте"
+		},
+
+		"asturias": {
+			"name": "Астуриас",
+			"application": "https://sede.asturias.es/Asturias/SERVICIOS/20021404_solicitud_7.pdf",
+			"law": "https://sede.asturias.es/portal/site/Asturias/menuitem.46a76b28f520ecaaf18e90dbbb30a0a0/?vgnextoid=9767c159b5d4f010VgnVCM100000b0030a0aRCRD&i18n.http.lang=es&_ga=2.107445569.2118992730.1519973757-1658900035.1519973757",
+			"where": "Специальный реестр - Edificio Buenavista C/ Eduardo Herrero “Herrerita”, s/n, 2ª planta. Oviedo"
+		},
+
+		"baleares": {
+			"name": "Балеарские острова",
+			"application": "http://www.caib.es/govern/rest/arxiu/1157576",
+			"law": "https://www.caib.es/seucaib/es/tramites/tramite/6958",
+			"where": "Специальный реестр - Dirección General de Menores y Familia. C/ Sant Joan de la Salle, 4. 07003 Palma de Mallorca."
+		},
+
+		"canarias": {
+			"name": "Канарские острова",
+			"application": "https://sede.gobcan.es/-/action/c002ea10-8036-11df-9987-52c39c0cc1c2/9515fb70-73a8-11df-8da5-b36e3e9e1a8c/es_ES/f79fd150-84fe-11df-8f9e-da38f72cc377/descargaDocumento?file=H3YZTXMGCR81T280LFR0&actuacion=3376&i3csrf=0503b2e0-1dfd-11e8-9913-d739bb3dcd4e&i3cg=23cbdd90-746b-11df-9b2d-33f09ca334e6&i3pt=C&i3ct=321b9560-73a9-11df-8da5-bf2cae36f426&i3l=es_ES&i3d=9515fb70-73a8-11df-8da5-b36e3e9e1a8c&i3sc&i3id=3376",
+			"law": "https://sede.gobcan.es/sede/procedimientos_servicios/tramites/3376?_ga=2.80656724.2118992730.1519973757-1658900035.1519973757#",
+			"where": "Специальные реестры - Dirección General de Administración Territorial y Gobernación. Avda. José Manuel Guimerá, 10. 38071 Santa Cruz de Tenerife. или " +
+					 "Dirección General de Administración Territorial y Gobernación. Prof. Agustín Millares Carló, 22. 35071 Las Palmas de Gran Canaria."
+		},
+		"cantabria": {
+			"name": "Кантаблия",
+			"application": "https://www.cantabria.es/documents/1564317/1564360/Anexo+I-Inscripcion+Basica2.pdf/21104833-0927-4a66-b900-e67ca881d49f",
+			"law": "https://www.cantabria.es/web/registro-de-parejas-de-hecho/registro-parejas-hecho/inscripcion-basica?_ga=2.183344485.2118992730.1519973757-1658900035.1519973757",
+			"where": "Специальный реестр - Consejería de Relaciones Institucionales y Asuntos Europeos. C/ Castelar, 5. 39004 Santander."
+		},
+		"castilla_lamancha": {
+			"name": "Кастийя - Ла Манча",
+			"application": "https://www.jccm.es/sites/www.jccm.es/files/modelos/SI4H_22216.PDF",
+			"law": "https://www.jccm.es/tramitesygestiones/registro-de-parejas-de-hecho?_ga=2.203914451.2118992730.1519973757-1658900035.1519973757",
+			"where": "Офисы Información y Registro в Вашем населённом пункте"
+		},
+		"castilla_leon": {
+			"name": "Кастийя и Леон",
+			"application": "https://www.tramitacastillayleon.jcyl.es/web/jcyl/binarios/574/785/IAPA_43_Solicitud%20Alta_@_v15.pdf?blobheader=application%2Fpdf%3Bcharset%3DUTF-8&blobheadername1=Cache-Control&blobheadername2=Expires&blobheadername3=Site&blobheadername4=Content-Disposition&blobheadervalue1=no-store%2Cno-cache%2Cmust-revalidate&blobheadervalue2=0&blobheadervalue3=Portal_Informador&blobheadervalue4=inline%3B+filename%3Dtramite.pdf&blobnocache=true",
+			"law": "https://www.tramitacastillayleon.jcyl.es/web/jcyl/AdministracionElectronica/es/Plantilla100DetalleFeed/1251181050732/Tramite/1201782392856/Tramite?_ga=2.185430250.2118992730.1519973757-1658900035.1519973757",
+			"where": "Delegacion Territorial в Вашей провинции"
+		},
+		"catalunya": {
+			"name": "Каталония",
+			"application": "http://justicia.gencat.cat/ca/tramits/tramits-temes/Registre-de-parelles-estables-de-Catalunya",
+			"law": "http://justicia.gencat.cat/ca/serveis/registre-parelles-estables/",
+			"where": "Офисы d'Informació i Atenció Ciutadana"
+		},
+		"valencia": {
+			"name": "Валенсия",
+			"application": "http://www.gva.es/downloads/publicados/IN/10072_BI.PDF?_ga=2.183006693.2118992730.1519973757-1658900035.1519973757",
+			"law": "http://www.gva.es/es/inicio/procedimientos?id_proc=917",
+			"where": "Специальные регистры - Registro Administrativo de Uniones de Hecho de Valencia: C/ Avellanas, 14 (bajo). 46003 Valencia, или" +
+					 "Dirección territorial de Justicia y Administraciones Públicas de Alicante: Rambla Méndez Núñez, 41 (Torre de la Generalitat) 03001 Alicante, или " +
+					 "Dirección territorial de Justicia y Administraciones Públicas de Castellón: C/ Mayor, 78. 12001 Castellón de la Plana."
+		},
+		"extremadura": {
+			"name": "Экстремадура",
+			"application": "http://www.juntaex.es/filescms/ddgg005/uploaded_files/PAREJAS_DE_HECHO/Solicitud_Parejas_de_Hecho_Modelo.doc",
+			"law": "http://www.juntaex.es/ddgg005/29",
+			"where": "Специальный реестр - C/ Antonio Rodríguez Moñino, 2. 06800 Mérida."
+		},
+		"galicia": {
+			"name": "Галисия",
+			"application": "https://sede.xunta.gal/doc-invia/rest/anexos/18462",
+			"law": "https://www.xunta.gal/dog/Publicados/2014/20141121/AnuncioG0244-171114-0001_es.html?_ga=2.106729664.2118992730.1519973757-1658900035.1519973757",
+			"where": "Administracións Públicas e Xustiza в Вашей провинции"
+		},
+		"rioja": {
+			"name": "Ла Риоха",
+			"application": "https://ias1.larioja.org//cex/sistemas/GenericoServlet?servlet=cex.sistemas.cap.portal.ImgServletSis&code=oumCvWIgBUGXelHYg2UTkPhXhSM%2FFmcHfWzRXFKOTdbApHyqPVxRsljcT6EwZHkZLEXZYSr1AOHP%0AqAL4kZKoF2BlcWgL%2FljV96z4LWY61NRftWgjq62VFdZd5OsNyWLWVoQ1UaSpmR3sDTgSj%2BtK1A%3D%3D",
+			"law": "http://www.larioja.org/larioja-client/cm/oficina-electronica?web=000&proc=19441",
+			"where": "Муниципалитет в вашей провинции"
+		},
+		"madrid": {
+			"name": "Мадрид",
+			"application": "http://www.aicode.org/FORMULARIOS/2013/Modelos%20impresos%20UNIONES%20DE%20HECHO%20Comunidad%20de%20Madrid.pdf",
+			"law": "http://www.madrid.org/cs/Satellite?c=CM_Tramite_FA&cid=1109168958196&definicion=Inscripcion+Registro&language=es&pagename=ComunidadMadrid/Estructura&pid=1109265444835&segmento=1&tipoServicio=CM_Tramite_FA&_ga=2.140335344.2118992730.1519973757-1658900035.1519973757",
+			"where": "Специальный реестр - Secretaría General Técnica de la Consejería de Justicia y Administraciones Públicas. C/ Gran Vía, 18. 28071 Madrid"
+		},
+		"murcia": {
+			"name": "Мурсия",
+			"application": "https://www.murcia.es/documents/11263/242163/ReglamentoParejasHecho.pdf",
+			"law": "http://www.murcia.es/web/portal/uniones-de-hecho?_ga=2.73830737.2118992730.1519973757-1658900035.1519973757",
+			"where": "Специальный реестр - Ubicación de la Oficina de Registro de Uniones de Hecho Glorieta de España, 1 Planta Baja. 30004 Murcia"
+		},
+		"navarra": {
+			"name": "Наварра",
+			"application": "https://sedeelectronica.pamplona.es/FichaTramite.aspx?id=20-47511",
+			"law": "https://www.navarra.es/NR/rdonlyres/9422C6C3-37E0-434B-925D-6BC7F0F595F3/303669/Registroparejasestables.pdf",
+			"where": "Специальный реестр - Oficina de Enlaces Civiles del Ayuntamiento de Pamplona"
+		},
+		"pais_vasco": {
+			"name": "Страна Басков",
+			"application": "http://www.euskadi.eus/contenidos/informacion/requisitos_documen_parejhecho/es_requidoc/adjuntos/Solicitud_Inscripci%C3%B3n.cast.pdf",
+			"law": "http://www.euskadi.eus/web01-a2famil/es/contenidos/informacion/requisitos_documen_parejhecho/es_requidoc/index.shtml?_ga=2.139295216.2118992730.1519973757-1658900035.1519973757",
+			"where": "Delegacion Territorial del Departamento de Vivienda y Asuntos Sociales в Вашей провинции"
+		}
+	}
+};
+
+Spain.showData = function(community) {
+	var data = this.data[community];
+
+	if (!data) {
+		return;
+	}
+
+	var results = $('<div class="resultsDiv"></div>');
+
+	var header = $('<h4>Регистрация гражданского брака в автономии <b>' + data.name + '</b></h4>');
+
+	results.append(header);
+	results.append($('<hr />'));
+
+	var lawLink = $('<div class="resultLink"></div>').append($('<a href="' + data.law + '" target="_blank">Законодательная база(на испанском языке), требования, списки документов</a>'));
+
+	results.append(lawLink);
+
+	var appLink = $('<div class="resultLink"></div>').append($('<a class="resultLink" href="' + data.application + '" target="_blank">Бланк заявления на регистрацию</a>'));
+
+	results.append(appLink);
+
+	var placeLink = $('<div class="resultLink"></div>');
+
+	placeLink.append($('<h5>Где регистрируют:</h5>'));
+	placeLink.append($('<span>' + data.where + '</span>'))
+
+	results.append(placeLink);
+
+	var closeButton = $('<button class="btn btn-primary btn-close">Закрыть</button>');
+
+	closeButton.click(
+		function () {
+			results.remove();
+		}
+	);
+
+	results.append(closeButton);
+
+	$('body').append(results);
+};
+
+$(document).ready(
+	function() {
+		$('.comunidad').on(
+			'click',
+			function(event) {
+				var community = event.target.getAttribute('data-community');
+
+				Spain.showData(community);
+			}
+		);
+	}
+);
